059a32a9b88e0d58762546225c61e00f49c88590
danindiana <danindiana@users.noreply.github.com>
2023-08-21 03:46:30 -0500

    Create pdf_js_divergence_calculator.py
    here's the refactored code with the text preprocessing step omitted:
    
    In this refactored code, the text preprocessing step has been removed, and the selected text from each PDF file is directly concatenated to form the selected_text variable. The rest of the program logic remains the same as in the previous version.
    
    
    This Python program performs the following steps:
    
    1. **Import Required Libraries**: The program starts by importing necessary libraries:
       - `os`: For operating system-related functionality.
       - `numpy as np`: For numerical computations and array operations.
       - `scipy.special.rel_entr`: For calculating the relative entropy between probability distributions.
       - `fitz` from the PyMuPDF library: For PDF processing.
    
    2. **Define Function to Find PDF Files**: The `find_pdf_files` function takes a directory path as input and scans it recursively to find all PDF files. It returns a list of paths to these PDF files.
    
    3. **Define Text Preprocessing Function**: The `preprocess_text` function takes in a text string and a target length. It truncates or pads the text to match the target length.
    
    4. **Define Jensen-Shannon Divergence Calculation Function**: The `calculate_js_divergence` function computes the Jensen-Shannon Divergence between two probability distributions `p` and `q`. This divergence measure quantifies the difference between the two distributions.
    
    5. **Define Function to Parse Selection**: The `parse_selection` function takes a user input string containing selected numbers or ranges and returns a list of selected indices.
    
    6. **Define Function to Extract Text from PDF**: The `extract_text_from_pdf` function takes a PDF file path as input and uses the PyMuPDF library to extract text from all pages of the PDF. The extracted text is then returned.
    
    7. **Define Function to Convert Windows Path to WSL Path**: The `convert_windows_path_to_wsl_path` function converts a Windows-style path to a WSL (Windows Subsystem for Linux) path, making it compatible for file operations.
    
    8. **Define Main Function**: The `main` function is where the program's main logic resides. It follows these steps:
       - Asks the user for the root directory (in Windows-style path) to scan and the target text length for preprocessing.
       - Converts the Windows-style path to a WSL path.
       - Uses the `find_pdf_files` function to locate PDF files within the specified directory.
       - If no PDF files are found, the program prints a message and exits.
       - Lists the found PDF files and prompts the user to select files by entering numbers or ranges.
       - The `parse_selection` function is used to parse the user's selection into a list of indices.
       - Iterates through the selected indices and extracts text from the corresponding PDF files.
       - Preprocesses the extracted text using the `preprocess_text` function.
       - Calculates the Jensen-Shannon Divergence between the preprocessed text of the first selected PDF and the rest of the selected PDFs.
       - Optionally, sorts and displays the calculated Jensen-Shannon Divergence scores.
       - Asks the user if they want to run the program again.
    
    9. **Execute the Program**: The `if __name__ == "__main__":` block ensures that the `main` function is executed only when the script is run directly (not when imported as a module).
    
    In summary, this program scans a specified directory for PDF files, allows the user to select specific files, computes the Jensen-Shannon Divergence between their preprocessed text contents, and offers options to sort and repeat the process. The Jensen-Shannon Divergence provides a measure of similarity/dissimilarity between the text content distributions of the selected PDFs.

---
+++ Jensen-Shannon Divergence/pdf_js_divergence_calculator.py
@@ -0,0 +1,103 @@
+import os
+import numpy as np
+from scipy.special import rel_entr
+import fitz
+
+def find_pdf_files(directory):
+    pdf_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith('.pdf'):
+                pdf_files.append(os.path.join(root, file))
+    return pdf_files
+
+def calculate_js_divergence(p, q):
+    m = 0.5 * (p + q)
+    js_divergence = 0.5 * np.sum(rel_entr(p, m)) + 0.5 * np.sum(rel_entr(q, m))
+    return js_divergence
+
+def parse_selection(selection):
+    ranges = selection.split(',')
+    selected_indices = []
+
+    for rng in ranges:
+        if '-' in rng:
+            start, end = map(int, rng.split('-'))
+            selected_indices.extend(range(start, end + 1))
+        else:
+            selected_indices.append(int(rng))
+    
+    return selected_indices
+
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    try:
+        doc = fitz.open(pdf_path)
+        for page in doc:
+            text += page.get_text()
+        doc.close()
+    except Exception as e:
+        print(f"Error extracting text from {pdf_path}: {e}")
+    return text
+
+def convert_windows_path_to_wsl_path(windows_path):
+    wsl_path = windows_path.replace("C:\\", "/mnt/c/").replace("\\", "/")
+    return wsl_path
+
+def main():
+    root_directory = input("Enter the root directory to start scanning (Windows-style path): ")
+
+    root_directory_wsl = convert_windows_path_to_wsl_path(root_directory)
+    pdf_files = find_pdf_files(root_directory_wsl)
+
+    if not pdf_files:
+        print("No PDF files found.")
+        return
+
+    print("PDF files found:")
+    for idx, file in enumerate(pdf_files, start=1):
+        print(f"{idx}.) {file}")
+
+    selected_input = input("Select files by entering numbers/ranges (e.g., 1-20,34,36,55,62-76,87,93-120): ")
+    selected_indices = parse_selection(selected_input)
+
+    selected_text = ""
+    selected_filenames = []
+    for idx in selected_indices:
+        if 0 <= idx - 1 < len(pdf_files):
+            pdf_path_wsl = pdf_files[idx - 1]
+            text = extract_text_from_pdf(pdf_path_wsl)
+            selected_text += text
+            selected_filenames.append(pdf_path_wsl)
+
+    if len(selected_filenames) < 2:
+        print("At least 2 PDF files are required for JS divergence calculation.")
+        return
+
+    p = np.array([ord(c) for c in selected_text])
+    q_list = [np.array([ord(c) for c in text]) for text in selected_text]
+
+    js_divergences = []
+    for q in q_list:
+        js_divergence = calculate_js_divergence(p, q)
+        js_divergences.append(js_divergence)
+
+    print("\nJensen-Shannon Divergence:")
+    for idx, (divergence, filename) in enumerate(zip(js_divergences, selected_filenames[1:]), start=1):
+        print(f"{idx}.) {filename}: {divergence:.4f}")
+
+    sort_choice = input("\nDo you want to sort JS divergence scores from highest to lowest? (y/n): ")
+    if sort_choice.lower() == "y":
+        sorted_indices = sorted(range(len(js_divergences)), key=lambda k: js_divergences[k], reverse=True)
+        print("\nSorted Jensen-Shannon Divergence:")
+        for idx, sorted_idx in enumerate(sorted_indices, start=1):
+            print(f"{idx}.) {selected_filenames[sorted_idx + 1]}: {js_divergences[sorted_idx]:.4f}")
+
+    choice = input("\nDo you want to go again? (yes/no): ")
+    if choice.lower() == "yes":
+        main()
+    else:
+        print("Exiting...")
+
+if __name__ == "__main__":
+    main()
