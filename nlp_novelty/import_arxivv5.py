fdbca581121783d2a56b039f87d20450e1c933d4
danindiana <danindiana@users.noreply.github.com>
2023-11-04 21:21:18 -0500

    Create import_arxivv5.py
    The code snippet provided in the refactoring example will write its output to a text file once for each category in the `cs_categories` list. It does not have any built-in mechanism to repeat at regular intervals.
    
    If you want the code to run at regular intervals and write its output to a text file each time, you would need to add a scheduling mechanism. In Python, you could use a loop with a sleep interval, or for a more robust solution, you could use a task scheduler like `cron` on Ubuntu to run your Python script at regular intervals.
    
    Here's how you could modify the Python script to run in an infinite loop with a sleep interval:
    
    ```python
    import arxiv
    import time
    from datetime import datetime
    
    # Create an instance of the Client
    client = arxiv.Client()
    
    # List of cs categories
    cs_categories = [
        # ... (same as before)
    ]
    
    while True:
        # Get the current date and time to use in the filename
        current_time = datetime.now()
        formatted_time = current_time.strftime("%Y-%m-%d_%H-%M-%S")
        filename = f"arxiv_cs_categories_{formatted_time}.txt"
    
        # Open the file for writing
        with open(filename, 'w') as file:
            # Loop through each category and perform the search
            for category in cs_categories:
                search = arxiv.Search(
                    query=f"cat:{category}",
                    max_results=50000,  # Reduced number of results for quicker execution
                    sort_by=arxiv.SortCriterion.SubmittedDate
                )
    
                try:
                    # Fetch the results using the client for each category
                    for result in client.results(search):
                        output = f"Category: {category}, Title: {result.title}\n"
                        print(output, end='')  # Print to console
                        file.write(output)  # Write to file
                        time.sleep(0.2)  # Sleep to avoid hitting rate limits
                except Exception as e:
                    error_message = f"An error occurred while processing category {category}: {e}\n"
                    print(error_message)
                    file.write(error_message)  # Write error message to file
                    time.sleep(10)  # Sleep before continuing to the next category
                    continue  # Continue with the next category
        
        # Sleep for a specified interval (e.g., 24 hours) before the next execution
        time.sleep(86400)
    ```
    
    In this modified version, the entire process is wrapped in a `while True:` loop, which will run indefinitely. At the end of each complete cycle, the script will sleep for a specified number of seconds (in this case, `86400` seconds, which is equivalent to 24 hours) before starting the next cycle.
    
    If you prefer not to keep a Python script running indefinitely, you could use the `cron` scheduler to execute your script at regular intervals. Here's an example of a `cron` job that runs every day at midnight:
    
    ```cron
    0 0 * * * /usr/bin/python3 /path/to/your/script.py
    ```
    
    This `cron` entry would need to be added to your crontab file, which you can edit by running `crontab -e` in the terminal. Remember to replace `/usr/bin/python3` with the path to your Python interpreter and `/path/to/your/script.py` with the path to your actual Python script.

---
+++ nlp_novelty/import_arxivv5.py
@@ -0,0 +1,47 @@
+import arxiv
+import time
+from datetime import datetime
+
+# Create an instance of the Client
+client = arxiv.Client()
+
+# List of cs categories
+cs_categories = [
+    "cs.AI", "cs.AR", "cs.CC", "cs.CE", "cs.CG", "cs.CL", "cs.CR", "cs.CV",
+    "cs.CY", "cs.DB", "cs.DC", "cs.DL", "cs.DM", "cs.DS", "cs.ET", "cs.FL",
+    "cs.GL", "cs.GR", "cs.GT", "cs.HC", "cs.IR", "cs.IT", "cs.LG", "cs.LO",
+    "cs.MA", "cs.MM", "cs.MS", "cs.NA", "cs.NE", "cs.NI", "cs.OH", "cs.OS",
+    "cs.PF", "cs.PL", "cs.RO", "cs.SC", "cs.SD", "cs.SE", "cs.SI", "cs.SY"
+]
+while True:
+    # Get the current date and time to use in the filename
+    current_time = datetime.now()
+    formatted_time = current_time.strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"arxiv_cs_categories_{formatted_time}.txt"
+
+    # Open the file for writing
+    with open(filename, 'w') as file:
+        # Loop through each category and perform the search
+        for category in cs_categories:
+            search = arxiv.Search(
+                query=f"cat:{category}",
+                max_results=50000,  # Reduced number of results for quicker execution
+                sort_by=arxiv.SortCriterion.SubmittedDate
+            )
+
+            try:
+                # Fetch the results using the client for each category
+                for result in client.results(search):
+                    output = f"Category: {category}, Title: {result.title}\n"
+                    print(output, end='')  # Print to console
+                    file.write(output)  # Write to file
+                    time.sleep(0.2)  # Sleep to avoid hitting rate limits
+            except Exception as e:
+                error_message = f"An error occurred while processing category {category}: {e}\n"
+                print(error_message)
+                file.write(error_message)  # Write error message to file
+                time.sleep(10)  # Sleep before continuing to the next category
+                continue  # Continue with the next category
+    
+    # Sleep for a specified interval (e.g., 24 hours) before the next execution
+    time.sleep(86400)
